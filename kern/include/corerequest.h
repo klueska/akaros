e90f4bb2f5397bb93f3248263a435b0580419c99
leymariv <valmon.leymarie@gmail.com>
2015-09-23 13:57:59 -0700

    First step to move corerequest API from schedule.c
    This is the first step to refactor the akaros kernel scheduler to move
    the core-provisioning and core-allocation APIs into 3 new files:
    corerequest.h, coreprov.c and corealloc.c. The corerequest.h file
    contains the full API for both allocating and provisioning cores, while
    the coreprov.c and corealloc.c files implement the provisiong and
    allocation APIs respectively.  We separate these implementation so that
    alternate core provisioning and core allocation strategies can be
    implemented without duplicating code.
    
    In the next commits we will eventually move the code for provisioning
    into coreprov.c and the code for core allocation into corealloc.c

---
+++ kern/include/corerequest.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2015 The Regents of the University of California
+ * Valmon Leymarie <leymariv@berkeley.edu>
+ * Kevin Klues <klueska@cs.berkeley.edu>
+ * See LICENSE for details.
+ */
+
+#ifndef AKAROS_KERN_COREREQUEST_H
+#define AKAROS_KERN_COREREQUEST_H
+
+#endif // AKAROS_KERN_COREREQUEST_H
---
+++ kern/src/corealloc.c
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2015 The Regents of the University of California
+ * Valmon Leymarie <leymariv@berkeley.edu>
+ * Kevin Klues <klueska@cs.berkeley.edu>
+ * See LICENSE for details.
+ */
